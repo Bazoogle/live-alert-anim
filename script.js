8e1d36ae73b3c3330d8b994769aa72c35c360776
Bazoogle <bazoogle@hotmail.com>
2019-03-23 23:40:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-var reqAnim = new HXLHttpRequest();
-reqAnim.open('GET', 'https://raw.githubusercontent.com/Bazoogle/live-alert-anim/master/animation1.json');
-reqAnim.onload = function () {
-	var animData = JSON.parse(reqAnim.responseText);
-};
-reqAnim.send();
-
 var animation = bodymovin.loadAnimation({
 	container: document.getElementById('anim'),
 	renderer: 'svg',
 	loop: false,
 	autoplay: true,
-	animationData: animData,
+	path: 'https://raw.githubusercontent.com/Bazoogle/live-alert-anim/master/animation1.json',
 });

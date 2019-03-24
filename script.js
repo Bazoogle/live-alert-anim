c71585a46dc654c30de1b6e2570460c62e5c92d1
Bazoogle <bazoogle@hotmail.com>
2019-03-24 12:11:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+setTimeout(fade_out, 8000);
+
+function fade_out() {
+  $("anim").fadeOut().empty();
+}
+
 var animation = bodymovin.loadAnimation({
 	container: document.getElementById('anim'),
 	renderer: 'svg',

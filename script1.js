b0b3118432efd7c76459be27f61c1ae750df9b46
Bazoogle <bazoogle@hotmail.com>
2019-03-27 06:06:22 -0400

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,8 @@
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('anim'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'https://cdn.jsdelivr.net/gh/Bazoogle/live-alert-anim/anim1.json',
+});

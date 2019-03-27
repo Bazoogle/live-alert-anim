998179886e1580302ff7fd7427638a081316542e
Bazoogle <bazoogle@hotmail.com>
2019-03-27 06:03:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-setTimeout(fade_out, 8000);
-
-function fade_out() {
-  $("anim").fadeOut().empty();
-}
-
 var animation = bodymovin.loadAnimation({
 	container: document.getElementById('anim'),
 	renderer: 'svg',
-	loop: false,
+	loop: true,
 	autoplay: true,
-	path: 'https://cdn.jsdelivr.net/gh/Bazoogle/live-alert-anim/animation1.json',
+	path: 'https://cdn.jsdelivr.net/gh/Bazoogle/live-alert-anim/anim1.json',
 });

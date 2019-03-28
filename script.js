348dcaa77a4434aaa10fe0509acb73304ea5af59
Bazoogle <bazoogle@hotmail.com>
2019-03-27 23:53:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+jQuery.loadScript = function (url, callback) {
+    jQuery.ajax({
+        url:'https://cdn.jsdelivr.net/gh/Bazoogle/live-alert-anim/test-demo-data.js',
+        dataType: 'script',
+        success: callback,
+        async: true
+    });
+}
+var text1 = "Bouncing text";
+
+test_demo_data.layers[1].t.d.k[0].s.t = text1;
+
 var animation = bodymovin.loadAnimation({
 	container: document.getElementById('anim'),
 	renderer: 'svg',
 	loop: true,
 	autoplay: true,
-	path: 'https://cdn.jsdelivr.net/gh/Bazoogle/live-alert-anim/anim1.json',
+		rendererSettings: {
+    	progressiveLoad: false
+	},
+	animationData: test_demo_data,
 });

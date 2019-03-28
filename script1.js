144b3ad001a83da13a0bbc30c46fec746ed32c1c
Bazoogle <bazoogle@hotmail.com>
2019-03-28 16:33:13 -0400

    Delete script1.js

--- script1.js
+++
@@ -1,8 +0,0 @@
-
-var animation = bodymovin.loadAnimation({
-	container: document.getElementById('anim'),
-	renderer: 'svg',
-	loop: true,
-	autoplay: true,
-	path: 'https://cdn.jsdelivr.net/gh/Bazoogle/live-alert-anim/anim1.json',
-});

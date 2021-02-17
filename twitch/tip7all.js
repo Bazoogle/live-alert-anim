ec6755d21aba05fc966f7a56567d3b0da2ae658c
Bazoogle <8096843+Bazoogle@users.noreply.github.com>
2021-02-17 00:20:48 -0500

    Update and rename tip6all.js to tip7all.js

--- twitch/tip7all.js
+++ twitch/tip7all.js
@@ -18,8 +18,8 @@ var tip1 = {"v":"5.7.6","fr":60,"ip":0,"op":990,"w":1920,"h":1080,"nm":"Donation
 var name =  document.getElementById('name').innerHTML;
 var text1 = name.toUpperCase();
 var amount =  document.getElementById('amount').innerHTML;
-var amount2 = parseInt(amount);
-var amount3 = amount2.toFixed(2);
+var amount2 = amount.replace("$", "");
+var amount3 = parseInt(amount2).toFixed(2);
 var amountFinal = "$"+amount3;
 
 console.log("Amount: "+amount+"Amount2: "+amount2+"Amount3: "+amount3)
